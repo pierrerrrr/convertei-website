e2b384146737f7358a16dfaa678ebd55c3de64e1
pierrerrrr <pierrestorniolo@gmail.com>
2025-09-01 17:57:11 -0300

    fix: change the main files to jsx to ease the website build

--- src/components/website/header/Header.jsx
+++ src/components/website/header/Header.jsx
@@ -17,17 +17,10 @@ function Header() {
             <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
                 {/* menu de navegação */}
                 <div className='flex justify-between items-center h-14'>
-                    {/* logo */}
                     <div className='flex-shrink-0'>
-                        <div className='flex items-center'>
-                            <div className="w-8 h-8 bg-gradient-to-r from-blue-500 to-purple-600 rounded-lg flex items-center justify-center mr-3">
-                                <span className="text-white font-bold text-lg">C</span>
-                            </div>
-                            <span className="text-xl font-bold tracking-tight">CONVERTEI</span>
-                        </div>
+                        {/* WIP: componentizar o logo, mais fácil pq vamo usar em uma pá de lugar */}
+                        {/* <Logo/> */}
                     </div>
-
-                    {/* anv */}
                     <div className='hidden md:flex items-center space-x-8'>
                         <nav className='flex space-x-6'>
                             {menuItems.map((item, index) => (
@@ -49,5 +42,3 @@ function Header() {
 }
 
 export default Header
-
-// WIP: componentizar o logo, mais fácil pq vamo usar em uma pá de lugar
